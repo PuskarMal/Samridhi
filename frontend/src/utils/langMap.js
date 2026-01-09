9be9241855fe43137e1269527de42f8bd2ea9114
PuskarMal <puskarmal07@gmail.com>
2026-01-09 14:03:34 +0530

    langauge mapping for speaker done

---
+++ frontend/src/utils/langMap.js
@@ -0,0 +1,18 @@
+export const langMap = {
+  en: "en-IN",
+  hi: "hi-IN",
+  bn: "bn-IN",
+  te: "te-IN",
+  mr: "mr-IN",
+  ta: "ta-IN",
+  ur: "ur-IN",
+  gu: "gu-IN",
+  kn: "kn-IN",
+  ml: "ml-IN",
+  or: "or-IN",
+  pa: "pa-IN",
+  as: "as-IN",
+  ne: "ne-NP",
+  sa: "sa-IN"
+};
+export const supportedLangs = Object.keys(langMap);
